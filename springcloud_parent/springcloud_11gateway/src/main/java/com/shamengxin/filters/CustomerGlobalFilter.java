3c79719db235a235bcf86d152f459408cd1ad6c4
shamengxin <1921028746@qq.com>
2023-04-13 19:35:25 +0800

    4.13

--- springcloud_parent/springcloud_11gateway/src/main/java/com/shamengxin/filters/CustomerGlobalFilter.java
+++ springcloud_parent/springcloud_11gateway/src/main/java/com/shamengxin/filters/CustomerGlobalFilter.java
@@ -1,4 +1,4 @@
-package com.shamengxin.filter;
+package com.shamengxin.filters;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
--- springcloud_parent/springcloud_12configserver/src/main/java/com/shamengxin/ConfigServerApplication.java
+++ springcloud_parent/springcloud_12configserver/src/main/java/com/shamengxin/ConfigServerApplication.java
@@ -2,12 +2,14 @@ package com.shamengxin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableConfigServer  //代表是统一配置中心服务
+@ServletComponentScan(basePackages = "com.shamengxin.filters")
 public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class,args);
